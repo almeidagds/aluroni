994aa9901af442fb0f887cb5d4086ba3d4508a9c
Gabriel de Almeida <almeidag2001@gmail.com>
2022-08-16 20:17:00 -0300

    Adicionando componente do buscador.

---
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./Buscador.module.scss";
+import { CgSearch } from "react-icons/cg";
+
+interface Props {
+    busca: string;
+    setBusca: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Buscador({ busca, setBusca }: Props) {
+    return (
+    <div className={styles.buscador}>
+        <input 
+            value={busca} 
+            onChange={(evento) => setBusca(evento.target.value)}
+        />
+        <CgSearch 
+            size={20}
+            color={"#4c4d5f"}
+        />
+    </div>
+    );
+}
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,8 +1,13 @@
 
 import styles from "./Cardapio.module.scss";
 import {ReactComponent as Logo} from "assets/logo.svg";
+import Buscador from "./Buscador";
+import { useState } from "react";
 
 export default function Cardapio() {
+
+    const [busca, setBusca] = useState("");
+
     return (
         <main>
             <nav className={styles.menu}>
@@ -11,6 +16,10 @@ export default function Cardapio() {
             <header className={styles.header}>
                 <div className={styles.header__text}>A casa do código e da massa</div>
             </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+                <Buscador busca={busca} setBusca={setBusca} />
+            </section>
         </main>
     );
 }
