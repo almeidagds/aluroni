eafecef98c7826a88aac00b9970616f2fcbcb43e
Gabriel de Almeida <almeidag2001@gmail.com>
2022-08-17 19:48:50 -0300

    Estilizando tags de categoria da lista de pratos.

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,17 +1,17 @@
 import styles from "./Item.module.scss";
-import logo from "assets/logo.svg";
 import cardapio from "../itens.json";
+import classNames from "classnames";
 
 type Props = typeof cardapio[0];
 
 export default function Item(props: Props) {
 
-    const { title, description, category, size, serving, price } = props;
+    const { title, description, category, size, serving, price, photo } = props;
 
     return (
         <div className={styles.item}>
             <div className={styles.item__imagem}>
-                <img src={logo} alt={title} />
+                <img src={photo} alt={title} />
             </div>
             <div className={styles.item__descricao}>
                 <div className={styles.item__titulo}>
@@ -20,7 +20,10 @@ export default function Item(props: Props) {
                 </div>
             </div>
             <div className={styles.item__tags}>
-                <div className={styles.item__tipo}>
+                <div className={classNames({
+                        [styles.item__tipo]: true,
+                        [styles[`item__tipo__${category.label.toLocaleLowerCase()}`]]: true
+                    })}>
                     {category.label}
                 </div>
                 <div className={styles.item__porcao}>
