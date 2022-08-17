eb0e2984cd6e8336422ebb34da10d9ebd79073a9
Gabriel de Almeida <almeidag2001@gmail.com>
2022-08-17 19:09:18 -0300

    Desenvolvendo estilos dinâmicos para o componente Ordenador.

--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -1,14 +1,36 @@
 import styles from "./Ordenador.module.scss";
 import opcoes from "./opcoes.json";
+import React, { useState } from "react";
+import classNames from "classnames";
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
 
-export default function Ordenador() {
+interface Props {
+    ordenador: string;
+    setOrdenador: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Ordenador({ ordenador, setOrdenador }: Props) {
+
+    const [aberto, setAberto] = useState(false);
+    const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome;
 
     return (
-        <button className={styles.ordenador}>
-            <span>Ordenar por</span>
-            <div className={styles.ordenador__options}>
+        <button className={classNames({
+                    [styles.ordenador]: true,
+                    [styles["ordenador--ativo"]]: ordenador !== ""
+                })} 
+                onClick={() => setAberto(!aberto)}
+                onBlur={() => setAberto(false)}>
+            <span>{nomeOrdenador || "Ordenar por"}</span>
+            {aberto ? <MdKeyboardArrowUp size={20}/> : <MdKeyboardArrowDown size={20}/>}
+            <div className={classNames({
+                [styles.ordenador__options]: true,
+                [styles["ordenador__options--ativo"]]: aberto
+            })}>
                 {opcoes.map(opcao => (
-                    <div className={styles.ordenador__option} key={opcao.value}>
+                    <div className={styles.ordenador__option} 
+                         key={opcao.value}
+                         onClick={() => setOrdenador(opcao.value)}>
                         {opcao.nome}
                     </div>
                 ))}
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -10,6 +10,7 @@ export default function Cardapio() {
 
     const [busca, setBusca] = useState("");
     const [filtro, setFiltro] = useState<number | null>(null);
+    const [ordenador, setOrdenador] = useState("");
 
     return (
         <main>
@@ -24,7 +25,7 @@ export default function Cardapio() {
                 <Buscador busca={busca} setBusca={setBusca} />
                 <div className={styles.cardapio__filtros}>
                     <Filtros filtro={filtro} setFiltro={setFiltro} />
-                    <Ordenador />
+                    <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
                 </div>
             </section>
         </main>
