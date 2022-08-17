a91b4bc7e02e4577dae5db59949e29ae3f9361ec
Gabriel de Almeida <almeidag2001@gmail.com>
2022-08-16 21:41:27 -0300

    Inserindo comportamento de ativar/desativar filtros.

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -1,18 +1,28 @@
+import React from "react";
 import filtros from "./filtros.json";
 import styles from "./Filtros.module.scss";
+import classNames from "classnames";
 
-export default function Filtros() {
+interface Props {
+    filtro: number | null;
+    setFiltro: React.Dispatch<React.SetStateAction<number | null>>;
+}
+
+export default function Filtros({ filtro, setFiltro }: Props) {
 
     type IOpcao = typeof filtros[0];
 
     function selecionarFiltro(opcao: IOpcao) {
-
+        return filtro === opcao.id ? setFiltro(null) : setFiltro(opcao.id);
     }
 
     return (
         <div className={styles.filtros}>
             {filtros.map((opcao) => (
-                <button className={styles.filtros__filtro} 
+                <button className={classNames({
+                            [styles.filtros__filtro]: true,
+                            [styles["filtros__filtro--ativo"]]: filtro === opcao.id
+                        })} 
                         key={opcao.id} 
                         onClick={() => selecionarFiltro(opcao)}>
                     {opcao.label}
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -8,6 +8,7 @@ import Filtros from "./Filtros";
 export default function Cardapio() {
 
     const [busca, setBusca] = useState("");
+    const [filtro, setFiltro] = useState<number | null>(null);
 
     return (
         <main>
@@ -21,7 +22,7 @@ export default function Cardapio() {
                 <h3 className={styles.cardapio__titulo}>Cardápio</h3>
                 <Buscador busca={busca} setBusca={setBusca} />
                 <div className={styles.cardapio__filtros}>
-                    <Filtros />
+                    <Filtros filtro={filtro} setFiltro={setFiltro} />
                 </div>
             </section>
         </main>
