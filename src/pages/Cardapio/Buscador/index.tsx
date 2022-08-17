80c04ac68b29824a5a612b1c8be20c9fca84fcd3
Gabriel de Almeida <almeidag2001@gmail.com>
2022-08-17 18:36:36 -0300

    Colocando placeholder na barra de pesquisa.

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -13,6 +13,7 @@ export default function Buscador({ busca, setBusca }: Props) {
         <input 
             value={busca} 
             onChange={(evento) => setBusca(evento.target.value)}
+            placeholder="Buscar . . ."
         />
         <CgSearch 
             size={20}
