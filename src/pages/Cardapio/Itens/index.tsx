3ac031747041269360cc89c752ee376f4fb3fa02
Gabriel de Almeida <almeidag2001@gmail.com>
2022-08-18 18:37:16 -0300

    Criando função responsável por ordenar os itens do cardápio.

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -25,10 +25,23 @@ export default function Itens(props: Props) {
         return true;
     }
 
+    function ordenar(novaLista: typeof cardapio) {
+        switch (ordenador) {
+            case "porcao":
+                return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
+            case "qtd_pessoas":
+                return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1);
+            case "preco":
+                return novaLista.sort((a, b) => a.price > b.price ? 1 : -1);
+            default:
+                return novaLista;
+        }
+    }
+
     useEffect(() => {
 
         const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
-        setLista(novaLista);
+        setLista(ordenar(novaLista));
 
     },[busca, filtro, ordenador]);
     
