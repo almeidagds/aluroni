9058918da2e39031eaec840bf66d37c22da544e1
Gabriel de Almeida <almeidag2001@gmail.com>
2022-08-20 14:07:35 -0300

    Configurando as rotas dos componentes.

---
+++ src/components/PaginaPadrao/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./PaginaPadrao.module.scss";
+import { Outlet } from "react-router-dom";
+
+export default function PaginaPadrao() {
+	return (
+		<>
+			<header className={styles.header}>
+				<div className={styles.header__text}>A casa do código e da massa</div>
+			</header>
+			<div>
+				<Outlet />
+			</div>
+		</>
+	);
+}
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,19 @@
 import Menu from "components/Menu";
+import PaginaPadrao from "components/PaginaPadrao";
 import Cardapio from "pages/Cardapio";
 import Inicio from "pages/Inicio";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import styles from "./Routes.module.scss";
 
 export default function AppRouter() {
 	return (
 		<main>
 			<Router>
 				<Menu />
-				<header className={styles.header}>
-					<div className={styles.header__text}>A casa do código e da massa</div>
-				</header>
 				<Routes>
-					<Route path="/" element={<Inicio />} />
-					<Route path="/cardapio" element={<Cardapio />} />
+					<Route path="/" element={<PaginaPadrao />}>
+						<Route index element={<Inicio />} />
+						<Route path="cardapio" element={<Cardapio />} />
+					</Route>
 				</Routes>
 			</Router>
 		</main>
