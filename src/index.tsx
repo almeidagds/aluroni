3076ac795a146a7f9ca34be74a6959f7f3f88bf9
Gabriel de Almeida <almeidag2001@gmail.com>
2022-08-20 12:20:58 -0300

    Instalando React Router e utilizando-o para determinar as URLs dos componentes.

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "normalize.css";
 import "./index.css";
-import Cardapio from "./pages/Cardapio";
-import Inicio from "pages/Inicio";
-
-const componenteAtual = window.location.pathname === "/" ? <Inicio /> : <Cardapio />;
+import AppRouter from "routes";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -13,6 +10,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
 	<React.StrictMode>
-		{componenteAtual}
+		<AppRouter />
 	</React.StrictMode>
 );
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import Cardapio from "pages/Cardapio";
+import Inicio from "pages/Inicio";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+export default function AppRouter() {
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Inicio />} />
+				<Route path="/cardapio" element={<Cardapio />} />
+			</Routes>
+		</Router>
+	);
+}
