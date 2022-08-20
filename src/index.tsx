a7dff65464670bfe4f126cfcc9a29398a650147b
Gabriel de Almeida <almeidag2001@gmail.com>
2022-08-20 12:06:24 -0300

    Criando condicional para visualização das páginas.

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "normalize.css";
 import "./index.css";
-//import Cardapio from "./pages/Cardapio";
+import Cardapio from "./pages/Cardapio";
 import Inicio from "pages/Inicio";
+
+const componenteAtual = window.location.pathname === "/" ? <Inicio /> : <Cardapio />;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
 	<React.StrictMode>
-		<Inicio />
+		{componenteAtual}
 	</React.StrictMode>
 );
