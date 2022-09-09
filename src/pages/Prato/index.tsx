93d76c71679c8e9c19affe02595c72dd81beeee3
Gabriel de Almeida <almeidag2001@gmail.com>
2022-09-08 22:06:53 -0300

    Criando página Prato e utilizando parâmetros do React Router.

---
+++ src/pages/Prato/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./Prato.module.scss";
+import { useParams } from "react-router-dom";
+
+export default function Prato() {
+	console.log(useParams());
+	return (
+		<div>
+
+		</div>
+	);
+}
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import PaginaPadrao from "components/PaginaPadrao";
 import Cardapio from "pages/Cardapio";
 import Inicio from "pages/Inicio";
 import NotFound from "pages/NotFound";
+import Prato from "pages/Prato";
 import Sobre from "pages/Sobre";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -19,6 +20,7 @@ export default function AppRouter() {
 						<Route path="sobre" element={<Sobre />} />
 					</Route>
 					<Route path="*" element={<NotFound />} />
+					<Route path="prato/:id" element={<Prato />} />
 				</Routes>
 				<Footer />
 			</Router>
