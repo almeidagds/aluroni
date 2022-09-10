a97c819944ee981b59f75d3814d668ee4c741bc5
Gabriel de Almeida <almeidag2001@gmail.com>
2022-09-10 12:37:23 -0300

    Melhorando a página de cardápio.

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,13 +1,15 @@
 import styles from "./Item.module.scss";
 import { Prato } from "types/Prato";
 import TagsPrato from "components/TagsPrato";
+import { useNavigate } from "react-router-dom";
 
 export default function Item(props: Prato) {
 
-	const { title, description,  photo } = props;
+	const { title, description,  photo, id } = props;
+	const navigate = useNavigate();
 
 	return (
-		<div className={styles.item}>
+		<div className={styles.item} onClick={() => navigate(`/prato/${id}`)}>
 			<div className={styles.item__imagem}>
 				<img src={photo} alt={title} />
 			</div>
--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -2,6 +2,7 @@ import styles from "./Prato.module.scss";
 import { useParams, useNavigate } from "react-router-dom";
 import cardapio from "data/cardapio.json";
 import TagsPrato from "components/TagsPrato";
+import NotFound from "pages/NotFound";
 
 export default function Prato() {
 	
@@ -10,7 +11,7 @@ export default function Prato() {
 	const prato = cardapio.find(item => item.id === Number(id));
 
 	if (!prato) {
-		return "";
+		return <NotFound />;
 	}
 
 	return (
